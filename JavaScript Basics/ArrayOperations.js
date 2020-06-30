d38d34c2dafec91e415d2a3c85d5625932aedfcf
Riya Prasad <ripras@microsoft.com>
2020-06-30 13:05:00 +0530

    Array operations and forms questions

---
+++ JavaScript Basics/ArrayOperations.js
@@ -0,0 +1,7 @@
+let countries = ["India", "USA", "Russia", "Japan"];
+countries.pop();
+countries.push();
+countries.shift();
+countries.unshift();
+countries.indexOf("India");
+//Refrence: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
